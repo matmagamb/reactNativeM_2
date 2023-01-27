3d7d690aad3ce1fac7c90e9bdb59e89719056c9b
Matheus Ambrosio <ceifa13@outlook.com>
2023-01-27 16:47:28 -0300

    definindo tema

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
+import { ThemeProvider } from "styled-components/native";
+
+
+
 
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
 
-    <Groups />
+      <Groups />
+    </ThemeProvider>
 
   );
 }
